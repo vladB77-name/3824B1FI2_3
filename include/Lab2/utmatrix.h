f5c730346d8b39ba2454fd805cf7d99503c47ba8
IvlevAlexander0 <180189657+IvlevAlexander0@users.noreply.github.com>
2025-09-25 18:59:47 +0300

    Create utmatrix.h

---
+++ include/Lab2/utmatrix.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+
+const int MAX_VECTOR_SIZE = 100000000;
+const int MAX_MATRIX_SIZE = 10000;
+
+// Шаблон вектора
+template <class ValType>
+class TVector
+{
+protected:
+  int Size;       // размер вектора
+  int StartIndex; // индекс первого элемента вектора
+  ValType* pVector;
+public:
+  TVector(int s = 10, int si = 0);
+  TVector(const TVector &v);                // конструктор копирования
+  ~TVector();
+  int GetSize()      { return Size;       } // размер вектора
+  int GetStartIndex(){ return StartIndex; } // индекс первого элемента
+  ValType& operator[](int pos);             // доступ
+  bool operator==(const TVector &v) const;  // сравнение
+  bool operator!=(const TVector &v) const;  // сравнение
+  TVector& operator=(const TVector &v);     // присваивание
+
+  // скалярные операции
+  TVector  operator+(const ValType &val);   // прибавить скаляр
+  TVector  operator-(const ValType &val);   // вычесть скаляр
+  TVector  operator*(const ValType &val);   // умножить на скаляр
+
+  // векторные операции
+  TVector  operator+(const TVector &v);     // сложение
+  TVector  operator-(const TVector &v);     // вычитание
+  ValType  operator*(const TVector &v);     // скалярное произведение
+
+  // ввод-вывод
+
+  friend std::istream& operator>>(std::istream& in, TVector& v);
+  friend std::ostream& operator<<(std::ostream& out, const TVector& v);
+};
+
+// Верхнетреугольная матрица
+template <class ValType>
+class TMatrix : public TVector<TVector<ValType>>
+{
+public:
+  TMatrix(int s = 10);                           
+  TMatrix(const TMatrix &mt);                    // копирование
+  TMatrix(const TVector<TVector<ValType>> &mt);  // преобразование типа
+  bool operator==(const TMatrix &mt) const;      // сравнение
+  bool operator!=(const TMatrix &mt) const;      // сравнение
+  TMatrix& operator= (const TMatrix &mt);        // присваивание
+  TMatrix  operator+ (const TMatrix &mt);        // сложение
+  TMatrix  operator- (const TMatrix &mt);        // вычитание
+
+  // ввод / вывод
+  friend std::istream& operator>>(std::istream& in, TMatrix& mt);
+  friend std::ostream& operator<<(std::ostream& out, const TMatrix& mt);
+};
